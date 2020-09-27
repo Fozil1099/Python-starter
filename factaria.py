f873715dd6c8da6a72e2d71975cdf99c474bd371
PreDatoR <MahmudjonovFozil@gmail.com>
2020-09-27 20:00:18 +0500

    1

--- factaria.py
+++ factaria.py
@@ -1,3 +1,5 @@
-import turtle
-tosh= turtle.turtle
-oyna = Turtle.screen
+k = int(input("takrorlanuvchi son: "))
+n = int(input("takrorlanuvchi son: "))
+for item in range(k+1, n):
+    s=n-item+k
+    print(s)
